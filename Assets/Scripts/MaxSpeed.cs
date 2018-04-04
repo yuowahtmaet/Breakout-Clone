da5587e45e6286bd4bbc62ba6c05910fee8de6b6
yuowahtmaet <anomalistic.curiosity@gmail.com>
2018-04-04 17:02:26 +1000

    Update

--- Assets/Scripts/MaxSpeed.cs
+++ Assets/Scripts/MaxSpeed.cs
@@ -6,6 +6,8 @@ public class MaxSpeed : MonoBehaviour
 {
 	[SerializeField]
 	private float maxSpeed;
+	[SerializeField]
+	private float minSpeed;
 	private Rigidbody2D rb;
 
 	void Start()
@@ -15,6 +17,16 @@ public class MaxSpeed : MonoBehaviour
 
 	void FixedUpdate ()
 	{
+		if (Mathf.Abs(rb.velocity.x) < minSpeed) 
+		{
+			float newVelocity = rb.velocity.x * 1.1f;
+			rb.velocity = new Vector2(newVelocity, rb.velocity.y);
+		}
+		if (Mathf.Abs(rb.velocity.y) < minSpeed) 
+		{
+			float newVelocity = rb.velocity.y * 1.1f;
+			rb.velocity = new Vector2(rb.velocity.x, newVelocity);
+		}
 		if(rb.velocity.magnitude > maxSpeed)
 		{
 			rb.velocity = rb.velocity.normalized * maxSpeed;
---
+++ Assets/SpawnOnCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnCollision : MonoBehaviour {
+
+	public GameObject preFab;
+
+	void OnCollisionExit2D (Collision2D col)
+	{
+		GameObject clone = Instantiate (preFab,transform.position, Quaternion.identity) as GameObject;
+		Destroy (gameObject);
+	}
+}
